637d84a7ed3a50b72ce52f85a4052ceff37db435
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-27 11:38:40 +0300

    create list view and it's url

--- product/models.py
+++ product/models.py
@@ -26,8 +26,13 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(default="")
     price = models.DecimalField(max_digits=7,decimal_places=2,default=0)
+    
     brand = models.CharField(max_length=200)
     category = models.CharField(max_length=30,choices=Category.choices)
     rating = models.DecimalField(max_digits=2,decimal_places=1)
+    stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
