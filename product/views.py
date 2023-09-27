abe36384146b3bf958f4b722a33b595f62a5c529
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-27 18:42:11 +0300

    create a serializer for list product

--- product/views.py
+++ product/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
-
+from .serializers import ProductSerializer
 @api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
+    
+    serializer = ProductSerializer(products,many = True)
+    return Response(serializer.data, status =status.HTTP_202_ACCEPTED)
 
-    return Response({"Mustafa" : "Ahmed"})
