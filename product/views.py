7bbf5378beeef9401334778cd96540028fd75ee8
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-28 19:55:42 +0300

    add filters

--- product/views.py
+++ product/views.py
@@ -4,17 +4,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer,ProductListSerializer
+from .filters import ProductFilter
 @api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
-    
-    serializer = ProductListSerializer(products,many = True, context={'request': request})
+    filterset = ProductFilter(request.GET , queryset=products)
+    serializer = ProductListSerializer(filterset.qs,many = True, context={'request': request})
     return Response(serializer.data, status =status.HTTP_202_ACCEPTED)
 
 @api_view(['GET'])
 def product_detail(request,uuid):
     products = get_object_or_404(Product,uuid=uuid)
-    
     serializer = ProductSerializer(products,context={'request': request})
     return Response(serializer.data, status =status.HTTP_202_ACCEPTED)
 
