cebe787fb6d4ae6177650e4526e0c631c100ae1d
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-04 21:01:22 +0300

    Create Profile model and passworReset view and password forgot

--- account/models.py
+++ account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,related_name='profile', on_delete=models.CASCADE)
+    reset_password_token = models.CharField(max_length=50,default="",blank=True)
+    reset_password_expire = models.DateTimeField(null=True,blank=True)
--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
+from django.shortcuts import get_object_or_404 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from .serializers import SignUpSerializer,UserSerializer
+from django.utils.crypto import get_random_string
+from datetime import datetime, timedelta
+from django.core.mail import send_mail
 
 
 @api_view(['POST'])
@@ -36,4 +40,52 @@ def user_update(request):
         return Response(serializer.data)
     else:
         return Response(serializer.errors)
+
+@api_view(['POST'])
+def password_forgot(request):
+    data = request.data
+    email = data.get('email')
+
+    if not email:
+        return Response({'error': 'Email is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = get_object_or_404(User, email=email)
+
+    token = get_random_string(40)
+    expire_date = datetime.now() + timedelta(minutes=30)
+
+    profile = user.profile
+    profile.reset_password_token = token
+    profile.reset_password_expire = expire_date
+    profile.save()
+
+    reset_link = f"http://localhost:8000/api/password/reset/{token}"
+    body = f"Your password reset link is: {reset_link}"
+
+    send_mail(
+        subject="Password reset from eMarket",
+        message=body,
+        from_email="eMarket@gmail.com",
+        recipient_list=[email]
+    )
+
+    return Response({'details': f'Password reset sent to {email}'})
+@api_view(['POST'])
+def password_reset(request,token):
+    data = request.data
+    user = get_object_or_404(User,profile__reset_password_token = token)
+
+    if user.profile.reset_password_expire.replace(tzinfo=None) < datetime.now():
+        return Response({'error': 'Token is expired'},status=status.HTTP_400_BAD_REQUEST)
+    
+    if data['password'] != data['confirmPassword']:
+        return Response({'error': 'Password are not same'},status=status.HTTP_400_BAD_REQUEST)
+    
+    user.password = make_password(data['password'])
+    user.profile.reset_password_token = ""
+    user.profile.reset_password_expire = None 
+    user.profile.save() 
+    user.save()
+    return Response({'details': 'Password reset done '})
+
     
